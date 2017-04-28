ce0aed6b0087f8611acb1b92c0f22e23374a9122
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-04-28 11:43:18 -0600

    Purpose, audience, goal, user story, persona, and use case completed - @GVanDyke84 @fellafox @abqmj

---
+++ documentation/one.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Custom CSS Below -->
+
+		<title>Dog Social</title>
+	</head>
+	<body>
+		<header>
+			<h1>Dog Social</h1>
+		</header>
+		<main>
+			<h2>Purpose, Audience, Goal, and User Story</h2>
+			<h3>Purpose:</h3>
+			<p>The purpose of our capstone project is to allow dog owners to feel a sense of community and feel safe. We want them to have a place to connect with other dog owners and stay informed about what is going on in their neighborhoods.</p>
+			<h3>Audience:</h3>
+			<p>The users visitng the site will be dog owners who enjoy outdoor activities.</p>
+			<h3>Goal:</h3>
+			<p>Our main goal is to increase positive interaction in the dog owner community. We want to be the go to hub for dog based businesses.</p>
+			<h3>User Story:</h3>
+			<p>As a user, a dog owner needs to be able to connect with other dog owners prior to exploring parks in a certain area.</p>
+			<h2>Persona</h2>
+			<p><strong>Name:</strong>Shawn<br><strong>Age:</strong>49</p>
+			<p><strong>Profession:</strong>HR Recruiter for Hilton</p>
+			<p><strong>Technology:</strong>PC computer and iPhone 6</p>
+			<p><strong>Attitudes and Behaviors:</strong>Extremely anxious and protective of her dog. She has a fear of huskies and doesn't want her dog near that breed.</p>
+			<p><strong>Frustrations and Needs:</strong>Shawn needs to be able to know what types of dogs frequent the dog parks she wants to go to. She is frustrated when she goes to dog parks and has to wait for certain dogs to leave before she can enter due to her fear of certain breeds.</p>
+			<p><strong>Goals:</strong>Shawn's goals are to be more active with her dog, meet other dog owners, and make going to dog parks more pleasant and less fearful.</p>
+			<h2>Use Case</h2>
+			<p>Shawn is working from home on a Tuesday afternoon. She is going to take a long lunch because she is overwhelmed from work and her boss' pressure. She's planning on taking her dog Finn on a walk. She is also dog sitting her roommate's dog Kota. She plans on taking the two of them to a local dog park in Phoenix, AZ. She goes to our website, and she logs into Finn's profile. She pulls up her list of favorite parks, and checks the visitor schedule. She notices there is a lab and a few mid size mixed breed dogs scheduled to be there within the next half hour. She packs some treats, water bowls, and she heads out with Finn and Kota for a fun adventure with new friends. While at the park, she opens our app and checks into the park, and she gets a check-in badge.</p>
+		</main>
+	</body>
+</html>
