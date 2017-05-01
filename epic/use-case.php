e51f74cfb5353d25ef6643a997f61fa5a3070e8c
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-01 13:48:06 -0600

    Completed Use Case but want to discuss another draft with team.

--- epic/use-case.php
+++ epic/use-case.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: littleloveprint
- * Date: 5/1/17
- * Time: 1:09 PM
- */
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<title>Use Case</title>
+	</head>
+	<body>
+		<header>
+			<h1>Use Case</h1>
+		</header>
+		<main>
+			<p>Shawn is working from home on a Tuesday afternoon. She is going to take a long lunch because she is overwhelmed from work and her boss' pressure. She's planning on taking her dog Finn on a walk. She is also dog sitting her roommate's dog Kota. She plans on taking the two of them to a local dog park in Phoenix, AZ. She goes to our website, and she logs into Finn's profile. She pulls up her list of favorite parks, and checks the visitor schedule. She notices there is a lab and a few mid size mixed breed dogs scheduled to be there within the next half hour. She packs some treats, water bowls, and she heads out with Finn and Kota for a fun adventure with new friends. While at the park, she opens our app and checks into the park, and she gets a check-in badge.</p>
+		</main>
+	</body>
+</html>
