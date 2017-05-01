fc00186e9c12922447df371cf8bb23fae074e871
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-01 14:57:31 -0600

    Great start on Conceptual Model w/ @abqmj @fellafox @GVanDyke84

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -13,31 +13,46 @@
 			<h2>Entities &amp; Attributes</h2>
 			<h3>Profile</h3>
 			<ul>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
+				<li>profileId</li>
+				<li>profileActivationToken</li>
+				<li>profileAtHandle</li>
+				<li>profileDogs</li>
+				<li>profileEmail</li>
+				<li>profileHash</li>
+				<li>profileSalt</li>
 			</ul>
-			<h3>Entity Name</h3>
+			<h3>Check-In</h3>
 			<ul>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
+				<li>checkInProfileId</li>
+				<li>checkInParkId</li>
+				<li>checkInDate</li>
 			</ul>
-			<h3>Entity Name</h3>
+			<h3>Dog</h3>
 			<ul>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
+				<li>dogId</li>
+				<li>dogProfileId</li>
+				<li>dogAge</li>
+				<li>dogBio</li>
+				<li>dogBreed</li>
+				<li>dogDogTag</li>
+				<li>dogPersonality</li>
+			</ul>
+			<h3>Park</h3>
+			<ul>
+				<li>parkId</li>
+				<li>parkLocation</li>
+				<li>parkReviews</li>
+				<li>parkRules</li>
+			</ul>
+			h3>Schedule</h3>
+			<ul>
+				<li>scheduleProfileId</li>
+				<li>scheduleParkId</li>
+				<li>scheduleDate</li>
 			</ul>
 			<h2>Relationships</h2>
 			<ul>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
+				<li>Many users can check into many parks.</li>
 			</ul>
 			<p><strong>Subject:</strong>Shawn<br><strong>Verb:</strong>Verb goes here<br><strong>Object:</strong>Object goes here</p>
 		</main>
