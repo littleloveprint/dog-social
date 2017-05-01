e5e0a4c502a8b15cfc85b17445bbcf925639094c
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-01 13:56:29 -0600

    Layout of conceptual model complete. Need to discuss with team.

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -1,7 +1,45 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: littleloveprint
- * Date: 5/1/17
- * Time: 1:09 PM
- */
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<title>Conceptual Model</title>
+	</head>
+	<body>
+		<header>
+			<h1>Conceptual Model</h1>
+		</header>
+		<main>
+			<h2>Entities &amp; Attributes</h2>
+			<h3>Profile</h3>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<h3>Entity Name</h3>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<h3>Entity Name</h3>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<h2>Relationships</h2>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<p><strong>Subject:</strong>Shawn<br><strong>Verb:</strong>Verb goes here<br><strong>Object:</strong>Object goes here</p>
+		</main>
+	</body>
+</html>
