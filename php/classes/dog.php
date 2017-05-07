9950f9ae00c302aabc832916d9be0e3bbe71ce1d
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-05-07 14:26:21 -0600

    got rid of errors and added accessor and mutator methods for dog bio https://app.asana.com/0/332296370771655/334790680173185

--- php/classes/dog.php
+++ php/classes/dog.php
@@ -64,22 +64,21 @@ Class dog implements \JsonSerializable {
 	 * @param string $newDogBreed containing an optional description of breed
 	 * @param string $newDogAtHandle containing the at handle for the dog
 	 * @throws \InvalidArgumentException if data types are not
-	 * @throws \RangeException if data values are out of bounds (eg. strings too 		long or negative integers.
+	 * @throws \RangeException if data values are out of bounds (eg. strings too      long or negative integers.
 	 * @throws \Exception if some other exception occurs
 	 * @documentation php.net
 	 */
 
 	public function __construct(?int $newDogId, int $newDogProfileId, tinyint $newDogAge, string $newDogCloudinaryId, string $newDogBio, string $newDogBreed, string $newDogAtHandle) {
 		try {
-				$this->setDogId($newDogId);
-				$this->setDogProfileId($newDogProfileId);
-				$this->setDogAge($newDogAge);
-				$this->setDogCloudinaryId($newDogCloudinaryId);
-				$this->setDogBio($newDogBio);
-				$this->setDogBreed($newDogBreed);
-				$this->setDogAtHandle($newDogAtHandle);
-		}
-		//determine what exception type was thrown
+			$this->setDogId($newDogId);
+			$this->setDogProfileId($newDogProfileId);
+			$this->setDogAge($newDogAge);
+			$this->setDogCloudinaryId($newDogCloudinaryId);
+			$this->setDogBio($newDogBio);
+			$this->setDogBreed($newDogBreed);
+			$this->setDogAtHandle($newDogAtHandle);
+		} //determine what exception type was thrown
 		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
@@ -103,103 +102,136 @@ Class dog implements \JsonSerializable {
 	 * @throws \TypeError if $newDogId is not an integer
 	 */
 
-public function setDogId(?int $newDogId): void {
-	//If dogId is null, return it.
-if($newDogId === null) {
-$this->dogId = null;
-return;
-}
+	public function setDogId(?int $newDogId): void {
+		//If dogId is null, return it.
+		if($newDogId === null) {
+			$this->dogId = null;
+			return;
+		}
 
 //verify the dogId is positive
-if($newDogId <= 0) {
-	throw(new \RangeException("dog Id is not positive"));
-}
+		if($newDogId <= 0) {
+			throw(new \RangeException("dog Id is not positive"));
+		}
 //convert and store dogId
-$this->dogId = $newDogId;
-}
+		$this->dogId = $newDogId;
+	}
 
 	/**
 	 * Accessor method for profile Id of the dog owner
 	 * This is a foreign key
 	 * @return int value of profileId
 	 */
-	public function $getDogProfileId() : int {
+	public function getDogProfileId(): int {
 		return ($this->dogProfileId);
-}
+	}
 
-/**
- * mutator method for profile Id of the dog owner
- * @param int $newProfileId value of the new id of the dog owner
- * @throws \RangeException if $newProfileId is not an int
- **/
-	public function setDogProfileId(int $newDogProfileId) : void {
+	/**
+	 * mutator method for profile Id of the dog owner
+	 * @param int $newProfileId value of the new id of the dog owner
+	 * @throws \RangeException if $newProfileId is not an int
+	 **/
+	public function setDogProfileId(int $newDogProfileId): void {
 		//verifies profile Id is positive
-	if($newDogProfileId <= 0) {
+		if($newDogProfileId <= 0) {
 			throw(new \RangeException("profile id is not positive"));
+		}
+		//convert and store profile id
+		$this->dogProfileId = $newDogProfileId;
 	}
-	//convert and store profile id
-	$this->dogProfileId = $newDogProfileId;
-}
 
-/**
- * accessor method for dog age
- * @return int of dogAge
- */
-	public function $getDogAge() : tinyint {
+	/**
+	 * accessor method for dog age
+	 * @return int of dogAge
+	 */
+	public function getDogAge(): tinyint {
 		return ($this->dogAge);
-}
-/**
- * mutator method for dogAge
- * @param int $newDogAge value of the new id of the dog owner
- */
+	}
 
-public function setDogAge(int $newDogAge) : void {
+	/**
+	 * mutator method for dogAge
+	 * @param int $newDogAge value of the new id of the dog owner
+	 */
+
+	public function setDogAge(int $newDogAge): void {
 		//verifies the dog age is positive
-	if($newDogAge <=0 {
+		if($newDogAge <= 0) {
 			throw(new \RangeException("dog age is not positive"));
+		}
+		//convert and store dogAge
+		$this->dogAge = $newDogAge;
 	}
-	//convert and store dogAge
-	$this->dogAge = $newDogAge;
-}
 
-/**
- * accessor method for dog cloudinaryId
- * @return string value of dog cloudinaryId
- */
-public function getDogCloudinaryId() {
-	return ($this->dogCloudinaryId);
-}
-/**
- * Mutator method for dog cloudinary Id
- * @param string $newDogCloudinaryId new value of dog cloudinary Id
- * @throws \InvalidArgumentException if $newDogCloudinaryId is insecure
- * @throws \RangeException if $newDogCloudinaryId is > 32 characters
- * @throws \TypeError if $newDogCloudinaryId isn't a string
- *
- */
-public function setDogCloudinaryId(string $newDogCloudinaryId = null) {
+	/**
+	 * accessor method for dog cloudinaryId
+	 * @return string value of dog cloudinaryId
+	 */
+	public function getDogCloudinaryId() {
+		return ($this->dogCloudinaryId);
+	}
+
+	/**
+	 * Mutator method for dog cloudinary Id
+	 * @param string $newDogCloudinaryId new value of dog cloudinary Id
+	 * @throws \InvalidArgumentException if $newDogCloudinaryId is insecure
+	 * @throws \RangeException if $newDogCloudinaryId is > 32 characters
+	 * @throws \TypeError if $newDogCloudinaryId isn't a string
+	 *
+	 */
+	public function setDogCloudinaryId(string $newDogCloudinaryId = null) {
 //if dog cloudinary id is null, return it
-if($newDogCloudinaryId === null) {
-		$this->dogCloudinaryId = null;
-}
+		if($newDogCloudinaryId === null) {
+			$this->dogCloudinaryId = null;
+		}
 //verify the dog cloudinary id is secure
-	$newDogCloudinaryId = trim($newDogCloudinaryId);
-	$newDogCloudinaryId = filter_var($newDogCloudinaryId. FILTER_SANITIZE_STRING. FILTER_FLAG_NO_ENCODE_QUOTES);
-	if(empty($newDogCloudinaryId) === true) {
-		throw(new \InvalidArgumentException("dog cloudinary id is empty or insecure"));
-	}
-	//Verify the dog cloudinary id will fit in the database
-	if(strlen($newDogCloudinaryId) > 32) {
-		throw(new \RangeException("dog cloudinary id is too large"));
-	}
+		$newDogCloudinaryId = trim($newDogCloudinaryId);
+		$newDogCloudinaryId = filter_var($newDogCloudinaryId . FILTER_SANITIZE_STRING . FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newDogCloudinaryId) === true) {
+			throw(new \InvalidArgumentException("dog cloudinary id is empty or insecure"));
+		}
+		//Verify the dog cloudinary id will fit in the database
+		if(strlen($newDogCloudinaryId) > 32) {
+			throw(new \RangeException("dog cloudinary id is too large"));
+		}
 //convert and store the dog cloudinary id
-	$this->dogCloudinaryId = $newDogCloudinaryId;
-}
+		$this->dogCloudinaryId = $newDogCloudinaryId;
+	}
+
+	/**
+	 * accessor method for dog bio
+	 * @return string value of dogBio
+	 */
+
+	public function getDogBio(): string {
+		return $this->dogBio;
+	}
+
+	/**
+	 * mutator method for dog bio
+	 * @param string $newDogBio string value of new dog bio
+	 * @throws \InvalidArgumentException if $newDogBio is not a string or insecure
+	 * @throws \RangeException if $newDogBio is > 255 characters
+	 * @throws \TypeError if $newDogBio is not a string
+	 *
+	 */
+
+	public function setDogBio(string $newDogBio): void {
+		//verify th dog bio is secure and a string
+		$newDogBio = trim($newDogBio);
+		$newDogBio = filter_var($newDogBio, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newDogBio) === true) {
+			throw(new \InvalidArgumentException("dog bio is empty or insecure"));
+		}
+		//verify the dog bio will fit in the database
+		if(strlen($newDogBio) > 255) {
+			throw(new \RangeException("dog bio is too long"));
+		}
+		//store dog bio
+		$this->dogBio = $newDogBio;
+	}
 
 
-
-
-public function jsonSerialize() {
+	public function jsonSerialize() {
 		return (get_object_vars($this));
+	}
 }
-
