f9da28b749b82f043092aed7cd94adab86152ea0
Emily Halleran <emily@blueroserevival.com>
2017-05-11 14:00:49 -0600

    Creation of Park Test

---
+++ php/classes/Test/ParkTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Edu\Cnm\BarkParkz\Test;
+
+use Edu\Cnm\BarkParkz\Park;
+
+//Grab the class under scrutiny
+require_once (dirname(__DIR__) . "/autoload.php");
+
+
+/**
+ * Full PHPUnit testx for the Park class
+ *
+ *This is a complete unit testx for the Park class. It is complete because all mySQL and PDO methods are tested for both invalid and valid inputs.
+ *
+ * @see Park
+ * @author Emily Rose Halleran
+ **/
+class ParkTest extends BarkParkzTest {
+/**
+ * Placeholder until account activation is created
+ * @var string $VALID_ACTIVATION
+ **/
+protected $VALID_ACTIVATION;
+}
