c7869a99ded7827326b8a1c2c78a5cdf96100d4f
Emily Halleran <emily@blueroserevival.com>
2017-05-18 15:38:07 -0600

    Got rid of the extra delete part

--- php/classes/Test/ParkTest.php
+++ php/classes/Test/ParkTest.php
@@ -106,42 +106,6 @@ class ParkTest extends BarkParkzTest {
 	public function testDeleteValidPark(): void {
 
 		// Count the number of rows, and save it for later
-		$numRows = $this->getConnection()->getRowcount("park");
-
-		// Create a new Park and insert into mySQL
-		$park = new Park(null, $this->VALID_PARKID, $this->VALID_PARKNAME, $this->VALID_LOCATIONX, $this->VALID_LOCATIONY);
-		$park->insert($this->getPDO());
-
-		// Edit the Park and update it in mySQL
-		$park->setParkID($this->VALID_PARKID);
-		$park->update($this->getPDO());
-
-// Grab the data from mySQL and enforce and the fields match our expectations
-		$pdoPark = Park::getParkByParkId($this->getPDO(), $park->getParkId());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("park"));
-		$this->assertEquals($pdoPark->getParkName(), $this->VALID_PARKNAME);
-		$this->assertEquals($pdoPark->getParkLocationX(), $this->VALID_LOCATIONX);
-		$this->assertEquals($pdoPark->getParkLocationY(), $this->VALID_LOCATIONY);
-	}
-
-	/**
-	 * Test updating a Park that does not exist
-	 *
-	 * @expectedException \PDOException
-	 **/
-	public function testUpdateInvalidPark() {
-
-		// Create a Park and try to update it without actually inserting it
-		$park = new Park(newParkId: null, $this->VALID_PARKID, $this->VALID_PARKNAME, $this->VALID_LOCATIONX, $this->VALID_LOCATIONY);
-$park->update($this->getPDO());
-}
-
-	/**
-	 * Test creating a Park and then deleting it
-	 **/
-	public function testDeleteValidPark(): void {
-
-		// Count the number of rows, and save it for later
 		$numRows = $this->getConnection()->getRowCount("park");
 
 		// Create a new Profile and insert it into mySQL
@@ -156,6 +120,18 @@ $park->update($this->getPDO());
 		$pdoPark = Park::getParkByParkId($this->getPDO(), $park->getParkId());
 		$this->assertEquals($numRows, $this->getConnection()->getRowCount("park"));
 	}
+
+	/**
+	 * Test updating a Park that does not exist
+	 *
+	 * @expectedException \PDOException
+	 **/
+	public function testUpdateInvalidPark() {
+
+		// Create a Park and try to update it without actually inserting it
+		$park = new Park(newParkId: null, $this->VALID_PARKID, $this->VALID_PARKNAME, $this->VALID_LOCATIONX, $this->VALID_LOCATIONY);
+$park->update($this->getPDO());
+}
 
 	/**
 	 * Test deleting a Park that does not exist
