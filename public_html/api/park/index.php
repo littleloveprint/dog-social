3caa703ac55506f550e42b361e349ba8d7d0765f
Emily Halleran <emily@blueroserevival.com>
2017-05-22 15:25:22 -0600

    https://app.asana.com/0/332296370771713/334790680173188
    Api for Park - Formatted

--- public_html/api/park/index.php
+++ public_html/api/park/index.php
@@ -6,7 +6,7 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\BarkParkz\{
-			Park,
+	Park,
 };
 
 /**
@@ -17,7 +17,7 @@ use Edu\Cnm\BarkParkz\{
 
 //verify the session, start if not active
 if(session_status() !== PHP_SESSION_ACTIVE) {
-			session_start();
+	session_start();
 }
 
 //prepare an empty reply
@@ -26,34 +26,34 @@ $reply->status = 200;
 $reply->data = null;
 
 try {
-			$pdo = connectToEncryptedMySQL("BIG/D");
+	$pdo = connectToEncryptedMySQL("BIG/D");
 
-			if($method === "GET") {
-				//set XSRF cookie
-				setXsrfCookie();
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
 
-				//gets all parks
-				if($parkId !== null && $parkName !== null) {
-					$park = Park::getParkByParkIdAndParkName($pdo, $parkId, $parkName);
+		//gets all parks
+		if($parkId !== null && $parkName !== null) {
+			$park = Park::getParkByParkIdAndParkName($pdo, $parkId, $parkName);
 
-					if($park !== null) {
-						$reply->data = $park;
-					}
-					//if none of the search parameters are met throw an exception
-				} else if(empty($parkId) === false) {
-					$park = Park::getParkByParkId($pdo, $parkId)->toArray();
-					if($park !== null) {
-						$reply->data = $park;
-					}
-					//get all the parks associated with the parkName
-				} else if(empty($parkName) === false) {
-					$park = Park::getParkByParkName($pdo, $parkName)->toArray();
-
-					if($park !== null) {
-						$reply->data = $park;
-					}
-					//if any other HTTP request is sent, throw an exception
-				} else {
-					throw new InvalidArgumentException("incorrect search parameters", 404);
-				}
+			if($park !== null) {
+				$reply->data = $park;
 			}
+			//if none of the search parameters are met throw an exception
+		} else if(empty($parkId) === false) {
+			$park = Park::getParkByParkId($pdo, $parkId)->toArray();
+			if($park !== null) {
+				$reply->data = $park;
+			}
+			//get all the parks associated with the parkName
+		} else if(empty($parkName) === false) {
+			$park = Park::getParkByParkName($pdo, $parkName)->toArray();
+
+			if($park !== null) {
+				$reply->data = $park;
+			}
+			//if any other HTTP request is sent, throw an exception
+		} else {
+			throw new InvalidArgumentException("incorrect search parameters", 404);
+		}
+	}
