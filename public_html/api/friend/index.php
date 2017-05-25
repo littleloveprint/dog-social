e805548f64d3e899aa82e071d819c2c921398613
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-25 13:02:04 -0600

    Sanitize, XSRF, composite, check.
    https://app.asana.com/0/332283240117427/347469445448763

--- public_html/api/friend/index.php
+++ public_html/api/friend/index.php
@@ -21,3 +21,30 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 
+// Prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	$pdo = connectToEncryptedMySQL("etc/apache2/capstone-mysql/barkparkz.ini");
+
+	// Mock a logged in user by mocking the session and assigning a specific user to it. This is only for testing purposes and should not be in the live code.
+	$_SESSION["profile"] = Profile::getProfileByProfileId($pdo, 722);
+
+	// Determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	// Sanitize the search parameters
+	$friendFirstProfileId = filter_input(INPUT_GET, "friendFirstProfileId", FILTER_VALIDATE_INT);
+	$friendSecondProfileId = filter_input(INPUT_GET, "friendSecondProfileId", FILTER_VALIDATE_INT);
+	if($method === "GET") {
+
+		// Set XSRF cookie
+		setXsrfCookie();
+
+		// Gets  a specific friend based on its composite key
+		if ($friendFirstProfileId !== null && $friendSecondProfileId !== null) {
+			$friend = Friend::getFriendByFriendFirstProfileIdAndFriendSecondProfileId($pdo, $friendFirstProfileId, $friendSecondProfileId);
+			if($friend!== null) {
+				$reply->data = $friend;
+			}
