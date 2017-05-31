14e03ce5268af5f5580b658be9a2086559c3d521
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-05-31 09:43:43 -0600

    sanitized and filtered inputs for dog api https://app.asana.com/0/332296370771655/347469445448760

--- public_html/api/dog/index.php
+++ public_html/api/dog/index.php
@@ -11,6 +11,38 @@ use Edu\Cnm\BarkParkz\Dog;
 /**
  * API for Dog class
  * @author Gerrit Van Dyke
+ *
  **/
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+//prepare empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//grab mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/barkparkz.ini");
+	//simulate a logged in profile by mocking the session and assigning a user to it
+	//for testing only, should not be in the live code
+	//$_SESSION["profile] = Profile::getProfileByProfileId($pdo, 732);
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] :
+	//sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$dogId = filter_input(INPUT_GET, "dogId",FILTER_VALIDATE_INT);
+	$dogProfileId = filter_input(INPUT_GET, "dogProfileId", FILTER_VALIDATE_INT);
+	$dogAge = filter_input(INPUT_GET, "dogAge", FILTER_VALIDATE_INT);
+	$dogCloudinaryId = filter_input(INPUT_GET, "dogCloudinaryId", FILTER_VALIDATE_INT);
+	$dogBio = filter_input(INPUT_GET, "dogBio", FILTER_VALIDATE_INT);
+	$dogBreed = filter_input(INPUT_GET, "dogBreed", FILTER_VALIDATE_INT);
+	$dogAtHandle = filter_input(INPUT_GET, "dogAtHandle", FILTER_VALIDATE_INT);
+if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("ID can't be empty or negative", 405));
+	}
+
+
+}
+
 
 
