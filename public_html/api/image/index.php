d60363a8be2106ef0dc19ea738bc2bf07878b61a
Michael Jordan <mikesjordan@gmail.com>
2017-05-31 13:47:50 -0600

    started image api @deepdivedylan unsure what to santize since i have no class https://app.asana.com/0/334403035277159/353610842523862

---
+++ public_html/api/image/index.php
@@ -0,0 +1,35 @@
+<?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\CNM\BarkParkz\{
+	Profile,
+	Dog
+};
+/**
+ * Api for the cloudinary images that will magically work with out a class YATA
+ *
+ * @autho Michael Jordan mj@mjcodes.com
+ **/
+
+//verify the session, start if inactive
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/barkparkz.ini");
+	//cloudinary api stuff
+	$config = readConfig("/etc/apache2/capstone-mysql/barkparkz.ini");
+	$cloudinary = json_decode($config["cloudinary"]);
+	\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
+	//determine which http method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	//sanitize input
+
+}
