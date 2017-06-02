5714647b8e3ca69ca9bddab1d86f692ec3033e06
Michael Jordan <mikesjordan@gmail.com>
2017-06-02 12:22:19 -0600

    namespaces messed up fixed now !
    https://app.asana.com/0/334403035277159/353610842523862

--- public_html/api/activation/index.php
+++ public_html/api/activation/index.php
@@ -2,7 +2,7 @@
 require_once dirname(__DIR__,3)."/php/classes/autoload.php";
 require_once dirname(__DIR__,3)."/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-use Edu\CNM\BarkParkz\Profile;
+use Edu\Cnm\BarkParkz\Profile;
 /**
  * API to check profile activation status
  * @author Michael Jordan mj@mjcodes.com
--- public_html/api/checkin/index.php
+++ public_html/api/checkin/index.php
@@ -3,9 +3,8 @@ require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-use Edu\CNM\BarkParkz\{
-	CheckIn,
-	Profile
+use Edu\Cnm\BarkParkz\{
+	CheckIn
 };
 /**
  * API for the CheckIn class
