e1b43af7b7818d32d40924dbb46033b05409dfc8
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-06-04 16:19:20 -0600

    fixed more bracing errors https://app.asana.com/0/332296370771655/347469445448760

--- public_html/api/dog/index.php
+++ public_html/api/dog/index.php
@@ -27,16 +27,17 @@ try {
 	//for testing only, should not be in the live code
 	//$_SESSION["profile] = Profile::getProfileByProfileId($pdo, 732);
 	//determine which HTTP method was used
-	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] :
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 		//sanitize input
 
-		$dogId = filter_input(INPUT_GET, "dogId", FILTER_VALIDATE_INT);
+	$dogId = filter_input(INPUT_GET, "dogId", FILTER_VALIDATE_INT);
 	$dogProfileId = filter_input(INPUT_GET, "dogProfileId", FILTER_VALIDATE_INT);
 	$dogAge = filter_input(INPUT_GET, "dogAge", FILTER_VALIDATE_INT);
 	$dogCloudinaryId = filter_input(INPUT_GET, "dogCloudinaryId", FILTER_VALIDATE_INT);
 	$dogBio = filter_input(INPUT_GET, "dogBio", FILTER_VALIDATE_INT);
 	$dogBreed = filter_input(INPUT_GET, "dogBreed", FILTER_VALIDATE_INT);
 	$dogAtHandle = filter_input(INPUT_GET, "dogAtHandle", FILTER_VALIDATE_INT);
+
 	if(($method === "DELETE" || $method === "PUT") && (empty($dogId) === true || $id < 0)) {
 		throw(new InvalidArgumentException("ID can't be empty or negative", 405));
 	}
@@ -111,28 +112,26 @@ try {
 			if(empty($_SESSION["profile"]) === true) {
 				throw(new \InvalidArgumentException("you must be logged in to post things about your dog", 403));
 			}
-				//create new dog and insert into database
-				$dog = new Dog(null, $requestObject->dogProfileId, $requestObject->dogAge, $requestObject->dogCloudinaryId, $requestObject->dogBio, $requestObject->dogBreed, $requestObject->dogAtHandle);
-				$dog->insert($pdo);
+			//create new dog and insert into database
+			$dog = new Dog(null, $requestObject->dogProfileId, $requestObject->dogAge, $requestObject->dogCloudinaryId, $requestObject->dogBio, $requestObject->dogBreed, $requestObject->dogAtHandle);
+			$dog->insert($pdo);
 
-				//update reply
-				$reply->message = "Dog created successfully";
-			} else {
-				throw (new InvalidArgumentException("Invalid HTTP method request"));
-			}
+			//update reply
+			$reply->message = "Dog created successfully";
 		}
+	} else {
+			throw (new InvalidArgumentException("Invalid HTTP method request"));
+	}
 
-}catch(\Exception | \TypeError $exception) {
-			$reply->status = $exception->getCode();
-			$reply->message = $exception->getMessage();
-		}
+} catch(\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
 
-		header("Content-type: application/json");
-		if($reply->data === null) {
-			unset($reply->data);
-		}
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
 
 // encode and return reply to front end caller
-	echo json_encode($reply);
-
-
+echo json_encode($reply);
