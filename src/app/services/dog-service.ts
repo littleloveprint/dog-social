19cccd63598165d521439aa2b1ca4e29d21ab777
Michael Jordan <mikesjordan@gmail.com>
2017-06-07 15:57:28 -0600

    fixed bracing errors in dog https://app.asana.com/0/334403035277159/359198542825354

--- src/app/services/dog-service.ts
+++ src/app/services/dog-service.ts
@@ -16,38 +16,39 @@ export class DogService extends BaseService {
 	private dogUrl = "api/dog/";
 
 	//call to dog api and create dog in question
-	createDog(dog: Dog): Observable<Status> {
+	createDog(dog : Dog) : Observable<Status> {
 		return (this.http.post(this.dogUrl, Dog))
 			.map(BaseService.extractMessage)
 			.catch(BaseService.handleError);
 	}
 
 	//call to dog api and edit dog in question
-	editDog(dog: Dog): Observable<Status> {
+	editDog(dog : Dog) : Observable<Status> {
 		return (this.http.put(this.dogUrl, Dog))
 			.map(BaseService.extractMessage)
 			.catch(BaseService.handleError);
-
+	}
 
 		//call to the dog api and get a dog by it's ID
-		getDog(id: number) : Observable <Dog> {
-			return(this.http.get(this.dogUrl + id)
+	getDog(dogId : number) : Observable <Dog> {
+		return(this.http.get(this.dogUrl + dogId)
 			.map(BaseService.extractData)
 			.catch(BaseService.handleError));
 	}
 
 		//call to the api and get a dog by profileId
-		getDogByProfileId(dogProfileId: number) :Observable <Dog[]> {
+	getDogByProfileId(dogProfileId : number) :Observable <Dog[]> {
 			return(this.http.get(this.dogUrl + dogProfileId)
 			.map(BaseService.extractData)
 			.catch(BaseService.handleError));
 	}
 
 // call to dog api and get dogs by dog breed
-		getDogByBreed(dogBreed: string) :Observable < Dog[] > {
+		getDogByBreed(dogBreed : string) : Observable < Dog[] > {
 			return(this.http.get(this.dogUrl + dogBreed)
 			.map(BaseService.extractData)
 			.catch(BaseService.handleError));
 	}
-	}
+
+
 }
