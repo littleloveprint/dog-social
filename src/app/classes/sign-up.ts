caa898ce8357d369f37ca141a80dffeb67c044e9
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-07 11:06:40 -0600

    Created sign-up.ts
    https://app.asana.com/0/332283240117427/359198542825352

---
+++ src/app/classes/sign-up.ts
@@ -0,0 +1,9 @@
+export class SignUp {
+	constructor(
+		public profileAtHandle: number,
+		public profileCloudinaryId: number,
+		public profileEmail: string,
+		public profilePassword: string,
+		public profilePasswordConfirm: string
+	) {}
+}
