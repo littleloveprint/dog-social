9a03275a85ece70ffb7d1b034f21998a1f249679
Michael Jordan <mikesjordan@gmail.com>
2017-06-07 11:47:54 -0600

    created checkIn-service for angular not sure its right https://app.asana.com/0/334403035277159/359198542825350

---
+++ src/app/services/checkIn-service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {BaseService} from "./base-service";
+import {Status} from "../classes/status";
+import {Tweet} from "../classes/tweet";
+import {Observable} from "rxjs/Observable";
+import {CheckIn} from "../classes/checkin";
+
+@Injectable ()
+export class CheckInService extends BaseService {
+	constructor(protected http:Http){
+		super(http);
+	}
+	//define the API endpoint
+	private checkInUrl ="api/CheckIn/";
+	//call to the CheckIn API and create the CheckIn in question
+	createCheckIn(checkIn: CheckIn) : Observable<Status> {
+		return(this.http.post(this.checkInUrl, CheckIn)
+			.map(BaseService.extractMessage)
+			.catch(BaseService.handleError));
+	}
+	//call to the CheckIn API and get a CheckIn object based on its Id
+	getCheckInByCheckInId(checkInId : number) : Observable<CheckIn> {
+		return(this.http.get(this.checkInUrl + checkInId)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
+	//call to the CheckIn API and get an array of CheckIns based off the CheckIn Dog Ids
+	getCheckInByCheckInDogId(checkInDogId : number) : Observable<CheckIn[]> {
+		return(this.http.get(this.checkInUrl + checkInDogId)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
+	getCheckInByCheckInParkId(checkInParkId : number) : Observable<CheckIn[]> {
+		return(this.http.get(this.checkInUrl + checkInParkId)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
+	getCheckInByCheckInDateTime(sunriseCheckInDateTime : Date ,sunsetCheckOutDateTime : Date) : Observable<CheckIn[]> {
+		return(this.http.get(this.checkInUrl + sunriseCheckInDateTime ,sunsetCheckOutDateTime)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+}
+}
