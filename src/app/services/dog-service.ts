efe05d11bc3051b3ddf69f316c827eeea16ba1e4
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-06-07 14:48:43 -0600

    fixed a typo https://app.asana.com/0/332296370771655/359198542825360

--- src/app/services/dog-service.ts
+++ src/app/services/dog-service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
 import {BaseService} from "./BaseService";
 import {Status} from "../classes/status";
-import {Tweet} from "../classes/tweet";
+import {Dog} from "../classes/dog";
 import {Observable} from "rxjs/Observable";
 
 @Injectable ()
