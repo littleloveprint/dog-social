d185a4417b813a7688484c71642788aeafbbb1cd
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-06-07 11:13:20 -0600

    finished dog.ts class https://app.asana.com/0/332296370771655/359198542825360

---
+++ src/app/classes/dog.ts
@@ -0,0 +1,11 @@
+export class Dog {
+	constructor(
+		public dogId: number,
+		public dogProfileId: number,
+		public dogAge: number,
+		public dogCloudinaryId: string,
+		public dogBio: string,
+		public dogBreed: string,
+		public dogAtHandle: string,
+	) {}
+}
