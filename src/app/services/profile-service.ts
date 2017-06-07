0a50546f706be5ab12a97766868e7b554666bb4b
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-07 10:38:24 -0600

    Created profile-service.ts
    https://app.asana.com/0/332283240117427/359198542825355

---
+++ src/app/services/profile-service.ts
@@ -0,0 +1,52 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {BaseService} from "./base-service";
+import {Status} from "../classes/status";
+import {Profile} from "../classes/profile";
+import {Observable} from "rxjs/Observable";
+
+@Injectable ()
+export class ProfileService extends BaseService {
+
+	constructor(protected http:Http) {
+		super(http);
+	}
+
+	// Define the API endpoint
+	private profileUrl = "api/profile/";
+
+	// Reach out to the profile API, and delete the profile in question.
+	deleteProfile(id: number) : Observable<Status> {
+		return(this.http.delete(this.profileUrl + id)
+			.map(BaseService.extractMessage)
+			.catch(BaseService.handleError));
+	}
+
+	// Call to the Profile API, and edit the profile in question.
+	editProfile(profile: Profile) : Observable<Status> {
+		return(this.http.put(this.profileUrl + profile.profileId, profile)
+			.map(BaseService.extractMessage)
+			.catch(BaseService.handleError));
+	}
+
+	// Call to the Profile API, and get a Profile object by its id.
+	getProfile(id: number) : Observable<Profile> {
+		return(this.http.get(this.profileUrl + id)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
+
+	// Call to the API to grab an array of profiles based on the user input.
+	getProfileByProfileAtHandle(profileAtHandle: string) :Observable<Profile[]> {
+		return (this.http.get(this.profileUrl + profileAtHandle)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
+
+	// Call to the profile API, and grab the corresponding profile by its email.
+	getProfileByProfileEmail(profileEmail: string) :Observable<Profile> {
+		return(this.http.get(this.profileUrl + profileEmail)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
+}
