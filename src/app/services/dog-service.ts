3b539bca0cdf3da72d355b8dcd63ec7d2571e4e1
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-06-07 14:43:21 -0600

    finished writing dogService but it is full of errors https://app.asana.com/0/332296370771655/359198542825360

--- src/app/services/dog-service.ts
+++ src/app/services/dog-service.ts
@@ -6,4 +6,47 @@ import {Tweet} from "../classes/tweet";
 import {Observable} from "rxjs/Observable";
 
 @Injectable ()
-export class dog-service extends
+export class DogService extends BaseService {
+
+	constructor(protected http:Http) {
+		super(http);
+	}
+
+	//define api endpoint
+	private dogUrl ="api/dog/";
+
+	// call to the dog api and edit the dog in question
+	editDog(dogId: number) : Observable<Status> {
+		return(this.http.put(this.dogUrl + dog.dogId, dog))
+			.map(BaseService.extractMessage)
+			.catch(BaseService.handleError);
+
+	}
+	//call to dog api and create dog in question
+	createDog(dog : Dog) : Observable<Status> {
+		return(this.http.post(this.dogUrl, dog))
+			.map(BaseService.handleError);
+	}
+
+	//call to the dog api and get a dog by it's ID
+	getDog(dogId : number) : Observable<Dog> {
+		return(this.http.get(this.dogUrl + dogId))
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError);
+	}
+
+	//call to the api and get a dog by profileId
+	getDogByProfileId(dogProfileId : number) : Observable>Dog[]> {
+		return(this.http.get(this.dogUrl + dogProfileId))
+		.map(BaseService.extractData)
+		.catch(BaseService.handleError);
+}
+
+// call to dog api and get dogs by dog breed
+getDogByBreed(dogBreed : string) : Observable<Dog[]> {
+		return(this.http.get(this.dogUrl + dogBreed))
+			.map(BaseService.extractData)
+	.catch(BaseService.handleError);
+}
+
+
