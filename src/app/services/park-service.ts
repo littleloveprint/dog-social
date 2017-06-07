d36e916e8647b2b84b596f0d510e026c929abc84
Emily Halleran <emily@blueroserevival.com>
2017-06-07 14:53:38 -0600

    https://app.asana.com/0/332296370771713/334790680173188
    Updated Angular park-service.ts deleted unnecessary sections

--- src/app/services/park-service.ts
+++ src/app/services/park-service.ts
@@ -15,41 +15,21 @@ export class parkService extends BaseService {
 	// Define the API endpoint.
 	private parkUrl = "/api/park/";
 
-	// Call to the park API, and delete the park in question.
-	deletePark(park :Park) : Observable<Status> {
-		return(this.http.put(this.parkUrl +park.parkId +park.parkLocationX +park.parkLocationY +park.parkName)
-			.map(BaseService.extractData)
-			.catch(BaseService.handleError));
-	}
-
-	// Call the friend API, and create a new park.
-	createPark(park : Park) : Observable<Status> {
-		return(this.http.post(this.parkUrl, park)
-			.map(BaseService.extractMessage)
-			.catch(BaseService.handleError));
-	}
-
-	// Grabs a specific park based on its composite key.
-	getParkByCompositeKey(parkLocationX : decimal parkLocationY : decimal parkName : number) : Observable <Park> {
-		return(this.http.get(this.parkUrl + parkLocationX + parkLocationY + parkName)
-			.map(BaseService.extractData)
-			.catch(BaseService.handleError));
-	}
-
-	getParkByParkId (parkId : number ) : Observable <Park[]> {
+	getParkByParkId (parkId : number) : Observable <Park[]> {
 		return(this.http.get(this.parkUrl + park.parkId)
 			.map(BaseService.extractData)
 			.catch(BaseService.handleError));
 	}
 
-	getParkByParkLocation (parkLocationX : decimal : parkLocationY : decimal) : Observable <Park[]> {
-		return(this.http.get(this.parkUrl + park.parkLocationX + park.parkLocationY)
-			.map(BaseService.extraData)
-			.catch(BaseService.handleError));
-}
-
-	getParkByParkName (parkName : string ) : Observable <Park[]> {
+	getParkByParkName (parkName : string) : Observable <Park[]> {
 		return(this.http.get(this.parkUrl + park.parkName)
 			.map(BaseService.extractData)
 			.catch(BaseService.handleError));
 	}
+
+	getAllParks() : Observable<Park[]> {
+				return(this.http.get(this.parkUrl)
+					.map(BaseService.extractData)
+					.catch(BaseService.extractData));
+	}
+}
