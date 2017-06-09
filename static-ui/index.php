15e51514cd9371101061d431dba0eb866cf2d228
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-09 10:23:17 -0600

    Added head and footer to landing page.

--- static-ui/index.php
+++ static-ui/index.php
@@ -1,24 +1,25 @@
-<?php require_once("");?>
-
-<!-- Landing Page for Bar Parkz
- * @author Lea McDuffie and Emily Halleran
- * @version 1.0 -->
+<?php require_once("library/head-utils.php");?>
 
 <body class="sfooter">
 	<div class="sfooter-content">
 
 		<!-- NO HEADER ON THIS PAGE -->
 
-		<button class="btn btn-up" type="button">SIGN UP</button>
-		<button class="btn btn-in" type="button">SIGN IN</button>
+		<div class="row">
+			<div class="col-sx-12">
+				<button class="btn btn-up" type="button">SIGN UP</button>
+				<button class="btn btn-in" type="button">SIGN IN</button>
 
-		<div class="fullscreen-bg">
-			<video loop muted autoplay poster="img/" class="fullscreen-bg__video">
-				<source src="video/" type="video/webm">
-			</video>
+				<div class="fullscreen-bg">
+					<video loop muted autoplay poster="img/" class="fullscreen-bg__video">
+						<source src="video/" type="video/webm">
+					</video>
+				</div>
+
+				<!-- FOOTER INSERT -->
+				<?php require_once("library/footer.php");?>
+			</div>
+			</div>
 		</div>
 
-		<!-- FOOTER INSERT -->
-		<?php require_once("");?>
-	</div>
 </body>
