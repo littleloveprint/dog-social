0291de1f27f02a87f2fbe8c79b60e022ff696d8e
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-09 09:50:37 -0600

    Created footer with PLACEHOLDER text.

---
+++ static-ui/library/footer.php
@@ -0,0 +1,12 @@
+<footer class="navbar-fixed-bottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				WORDS FOR YOUR FOOT
+			</div>
+			<div class="col-sm-6 text-right-sm">
+				WHAT!?
+			</div>
+		</div>
+	</div>
+</footer>
