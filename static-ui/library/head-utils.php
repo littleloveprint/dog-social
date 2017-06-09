7d0719466e908e03563ccbdae48f40b983825ef7
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-09 10:21:56 -0600

    Head edit.

--- static-ui/library/head-utils.php
+++ static-ui/library/head-utils.php
@@ -5,9 +5,11 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="Portfolio website for Lea McDuffie">
-		<meta name="author" content="Lea McDuffie">
-		<meta name="version" content="4.0">
+		<meta name="description" content="social networking website for dog lovers">
+		<meta name="author" content="Emily Halleran, Gerrit Van Dyke, Lea McDuffie, Michael Jordan">
+		<meta name="version" content="1.0">
 
 		<!-- GOOGLE FONTS -->
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+
+	</head>
