9041a36e87088a1514285522f8233bafa458e1bb
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-09 09:48:30 -0600

    Created 404 page.

---
+++ static-ui/404.php
@@ -0,0 +1,21 @@
+<?php require_once("");?>
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- HEADER AND NAVBAR INSERT -->
+		<?php require_once("");?>
+
+		<main class="bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 text-center">
+						<h1>404 LOST PUPPY.</h1>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div><!--/.sfooter-content-->
+
+	<!-- FOOTER INSERT -->
+	<?php require_once("");?>
+</body>
