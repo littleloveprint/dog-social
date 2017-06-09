e37a6f9bdf858fd9a343c746c6e98730957fb97b
Michael Jordan <mikesjordan@gmail.com>
2017-06-09 11:28:12 -0600

    routes and modules updated                                                        https://app.asana.com/0/334403035277159/359198542825358

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
+import {CheckInService} from "./services/checkIn.service";
+import {DogService} from "./services/dog.service";
+import {FavoriteService} from "./services/favorite.service";
+import {FriendService} from "./services/friend.service";
+import {ImageService} from "./services/image.service";
+import {ParkService} from "./services/park.service";
+import {ProfileService} from "./services/profile.service";
+import {SignInService} from "./services/sign-in.service";
+import {SignUpService} from "./services/sign-up.service";
+import {SignOutService} from "./services/sign-out.service";
 
 const moduleDeclarations = [AppComponent];
 
@@ -11,6 +21,6 @@ const moduleDeclarations = [AppComponent];
 	imports:      [BrowserModule, FormsModule, HttpModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
-	providers:    [appRoutingProviders]
+	providers:    [appRoutingProviders, SignInService, SignOutService, SignUpService, ProfileService, ParkService, ImageService, FriendService, FavoriteService, DogService, CheckInService]
 })
 export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
+import {SignUpComponent} from "./components/sign-up.component";
+import {SignInComponent} from "./components/sign-in.component";
 
 
-export const allAppComponents = [HomeComponent];
+export const allAppComponents = [HomeComponent, SignUpComponent, SignInComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent}
