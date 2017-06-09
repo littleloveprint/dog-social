9b7aa29d5ec84b04c2f7071f57471ff971d004ba
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-09 09:45:53 -0600

    Created home page.

---
+++ static-ui/index.php
@@ -0,0 +1,21 @@
+<?php require_once("");?>
+
+<!-- Landing Page for Bar Parkz
+ * @author Lea McDuffie and Emily Halleran
+ * @version 1.0 -->
+
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- NO HEADER ON THIS PAGE -->
+
+		<div class="fullscreen-bg">
+			<video loop muted autoplay poster="img/" class="fullscreen-bg__video">
+				<source src="video/" type="video/webm">
+			</video>
+		</div>
+
+		<!-- FOOTER INSERT -->
+		<?php require_once("");?>
+	</div>
+</body>
