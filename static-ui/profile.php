8a2c01cc6dbae109e5b492cdf691c0c61b5f433e
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-10 10:52:08 -0600

    Created profile.

---
+++ static-ui/profile.php
@@ -0,0 +1,10 @@
+<!-- HEAD -->
+<?php require_once("library/head-utils.php");?>
+
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- HEADER AND NAVBAR -->
+		<?php require_once("library/header.php");?>
+
+		<main class=""
