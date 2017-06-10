6eb4812ee0ea6b5cd36051030a11267d8f3850a3
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-10 14:11:39 -0600

    Added footer component.

---
+++ src/app/components/footer.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Observable} from "rxjs";
+
+@Component({
+	selector: "meow-footer",
+	templateUrl: "./template/footer.php"
+})
+
+export class FooterComponent {}
