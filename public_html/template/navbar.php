c161ecfee78950bda818741d16d341c445b0e35c
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-10 14:31:35 -0600

    Created navbar template.

---
+++ public_html/template/navbar.php
@@ -0,0 +1,30 @@
+<header>
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+
+			<!-- MOBILE DISPLAY -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="">BARK PARKZ</a>
+			</div>
+
+			<!-- COLLECT LINKS, FORMS AND OTHER CONTENT FOR TOGGLING -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href=""><i class="fa fa-home"></i></a></li>
+					<li><a href="about.php">About</a></li>
+					<li><a href="profile.php">Profile</a></li>
+					<li><a href="dog.php">Dog</a></li>
+					<li><a href="parks.php">Parks</a> </li>
+				</ul>
+
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+</header>
