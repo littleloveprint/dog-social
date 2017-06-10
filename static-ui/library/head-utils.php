35ebffc4ac08709636e5d22231cf3a8106fa8f39
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-10 13:47:53 -0600

    Moved CSS and JavaScript from index to head-utils -- called in require_once

--- static-ui/library/head-utils.php
+++ static-ui/library/head-utils.php
@@ -12,4 +12,22 @@
 		<!-- GOOGLE FONTS -->
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
 
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta http-equiv="x-ua-compatible" content="ie=edge">
+			<!--custom CSS-->
+			<link rel="stylesheet" type="text/css" href="../static-ui/css/style.css">
+			<!-- Latest compiled and minified CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+
+			<!-- Latest compiled and minified JavaScript -->
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+			<!-- jQuery -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		</head>
+
 	</head>
