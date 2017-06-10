560a11252d6865c861632d1ab84c786857f91603
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-10 13:57:29 -0600

    More comments.

--- static-ui/index.php
+++ static-ui/index.php
@@ -1,3 +1,4 @@
+<!-- HEAD INSERT -->
 <?php require_once("library/head-utils.php");?>
 
 <body class="sfooter">
@@ -7,12 +8,14 @@
 
 		<div class="row">
 			<div class="col-sx-12">
-				<button class="btn btn-up" type="button">SIGN UP</button>
-				<button class="btn btn-in" type="button">SIGN IN</button>
+				<div class="align-right">
+					<button class="btn btn-up" type="button">SIGN UP</button>
+					<button class="btn btn-in" type="button">SIGN IN</button>
+				</div>
 
 				<div class="fullscreen-bg">
 					<video loop muted autoplay poster="img/BarkParkzTransparent.png" class="fullscreen-bg__video">
-						</video>
+					</video>
 				</div>
 				<div class="video-background">
 					<div class="video-foreground">
--- static-ui/library/head-utils.php
+++ static-ui/library/head-utils.php
@@ -13,13 +13,14 @@
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
 
 			<!--custom CSS-->
-			<link rel="stylesheet" type="text/css" href="../static-ui/css/style.css">
+			<link rel="stylesheet" type="text/css" href="../css/style.css">
 
 			<!-- Latest compiled and minified CSS -->
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 			<!-- Latest compiled and minified JavaScript -->
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 			<!-- jQuery -->
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
