c54a16e8b8358e572958327fa0ef49c247455fe5
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-10 15:12:30 -0600

    Put buttons in a container.

--- static-ui/index.php
+++ static-ui/index.php
@@ -6,27 +6,35 @@
 
 		<!-- NO HEADER ON THIS PAGE -->
 
-		<div class="row">
-			<div class="col-sx-12">
-				<div class="align-right">
-					<button class="btn btn-up" type="button">SIGN UP</button>
-					<button class="btn btn-in" type="button">SIGN IN</button>
-				</div>
-
-				<div class="fullscreen-bg">
-					<video loop muted autoplay poster="img/BarkParkzTransparent.png" class="fullscreen-bg__video">
-					</video>
-				</div>
-				<div class="video-background">
-					<div class="video-foreground">
-						<iframe src="https://www.youtube.com/embed/_uTuRIxAgZ4?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=_uTuRIxAgZ4" frameborder="0" allowfullscreen></iframe>
+		<div class="container container-fluid">
+			<div class="row">
+				<div class="col-sx-12">
+					<div class="align-right">
+						<button class="btn btn-up" type="button">SIGN UP</button>
+						<button class="btn btn-in" type="button">SIGN IN</button>
 					</div>
+
+					<div class="fullscreen-bg">
+						<video loop muted autoplay poster="img/BarkParkzTransparent.png" class="fullscreen-bg__video">
+						</video>
+					</div>
+					<div class="video-background">
+						<div class="video-foreground">
+							<iframe src="https://www.youtube.com/embed/_uTuRIxAgZ4?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=_uTuRIxAgZ4" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+
+
+
+
+
+
 				</div>
+			</div>
+		</div><!-- /.container -->
 
 				<!-- FOOTER INSERT -->
 				<?php require_once("library/footer.php");?>
 			</div>
-		</div>
-	</div>
 
 </body>
