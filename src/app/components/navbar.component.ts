83d079f12305d33323b9efc3c51b0122975efca8
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-10 14:07:40 -0600

    Added navbar component.

---
+++ src/app/components/navbar.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Observable} from "rxjs";
+
+@Component({
+	selector: "barkparkz-navbar",
+	templateUrl: "./templates/navbar.php"
+})
+
+export class NavbarComponent {}
