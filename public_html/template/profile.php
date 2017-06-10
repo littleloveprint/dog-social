f7fd443602ca4f54581ab569de723b57a507db44
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-10 11:11:25 -0600

    Created profile template

--- public_html/template/profile.php
+++ public_html/template/profile.php
@@ -1,17 +1,7 @@
-<!-- HEAD -->
-<?php require_once("library/head-utils.php");?>
-
-<body class="sfooter">
-	<div class="sfooter-content">
-
-		<!-- HEADER AND NAVBAR -->
-		<?php require_once("library/header.php");?>
-
-		<main class="class">
-			CLOUDINARY ID<br>
-			@HANDLE
-
-			<?php require_once("library/footer.php");?>
-		</main>
-	</div>
-</body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: littleloveprint
+ * Date: 6/10/17
+ * Time: 11:10 AM
+ */
---
+++ static-ui/profile.php
@@ -0,0 +1,17 @@
+<!-- HEAD -->
+<?php require_once("library/head-utils.php");?>
+
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- HEADER AND NAVBAR -->
+		<?php require_once("library/header.php");?>
+
+		<main class="class">
+			CLOUDINARY ID<br>
+			@HANDLE
+
+			<?php require_once("library/footer.php");?>
+		</main>
+	</div>
+</body>
