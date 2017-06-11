b6dadd6e2d8f65a22fa52d8d10792a527e9c3301
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-11 08:27:15 -0600

    Added NavbarComponent and FooterComponent to routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
 import {SignUpComponent} from "./components/sign-up.component";
 import {SignInComponent} from "./components/sign-in.component";
+import {NavbarComponent} from "./components/navbar.component";
+import {FooterComponent} from "./components/footer.component";
 
 
-export const allAppComponents = [HomeComponent, SignUpComponent, SignInComponent];
+export const allAppComponents = [HomeComponent, SignUpComponent, SignInComponent, NavbarComponent, FooterComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent}
