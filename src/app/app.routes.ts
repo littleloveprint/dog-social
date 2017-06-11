7b1fe1a268c027a37c29257d08607f12f2e47a28
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-11 09:04:40 -0600

    Added PostService to routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,15 @@ import {SignUpComponent} from "./components/sign-up.component";
 import {SignInComponent} from "./components/sign-in.component";
 import {NavbarComponent} from "./components/navbar.component";
 import {FooterComponent} from "./components/footer.component";
-
+import {PostService} from "./services/post.service";
 
 export const allAppComponents = [HomeComponent, SignUpComponent, SignInComponent, NavbarComponent, FooterComponent];
 
 export const routes: Routes = [
-	{path: "", component: HomeComponent}
+	{path: "", component: HomeComponent},
+	{path: "**", redirectTo: ""}
 ];
 
-export const appRoutingProviders: any[] = [];
+export const appRoutingProviders: any[] = [PostService];
 
 export const routing = RouterModule.forRoot(routes);
