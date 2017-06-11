0ec664234c9d5c4daa2ceec5b2e0a090149fb713
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-11 08:54:16 -0600

    Working backwards -- Created post.service.ts

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {BaseService} from "./base.service";
+import {Post} from "../classes/post";
+import {Status} from "../classes/status";
+
+@Injectable()
+export class PostService extends BaseService {
+	constructor(protected http: Http) {
+		super(http);
+	}
+
+	private postUrl = ".api/post";
+
+	getAllPosts() : Observable<Post[]> {
+		return(this.http.get(this.postUrl)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getPostByPostId(postId : number) : Observable<Post> {
+		return(this.http.get(this.postUrl + postId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	createPost(post : Post) : Observable<Status> {
+		return(this.http.post(this.postUrl, post)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+}
