83c3d6d7314fce7bb4389e9781b16f9862c6385f
Michael Jordan <mikesjordan@gmail.com>
2017-06-12 11:17:14 -0600

    dog template and updated component https://app.asana.com/0/334403035277159/359198542825358

---
+++ public_html/template/dog.php
@@ -0,0 +1,17 @@
+<div class="fullscreen-bg">
+	<img src="" alt="Background Image">
+</div>
+
+<main class="class">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-2 col-md-4">
+				<p>WE WANT THE CLOUDINARY img HERE</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-offset-2 col-md-4">
+				<p id="dog-handle"> WOOF WOOF {{ dog.dogAtHandle }}</p>
+			</div>
+		</div>
+	</div>
--- src/app/components/dog.component.ts
+++ src/app/components/dog.component.ts
@@ -1,9 +1,26 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Params} from "@angular/router";
+import {DogService} from "../services/dog.service"
+import {Status} from "../classes/status";
+import {Dog} from "../classes/dog";
+import "rxjs/add/operator/switchMap";
 import {Observable} from "rxjs";
 
 @Component({
 	templateUrl: "./template/dog.php"
 })
 
-export class DogComponent {}
+export class DogComponent implements OnInit{
+	status: Status = null;
+	dog: Dog = new Dog(null, null, null, null, null, null, null);
+	constructor(private dogService :DogService, private route :ActivatedRoute){
+	}
+	ngOnInit(): void {
+		this.getCurrentDog();
+	}
+	getCurrentDog(): void{
+		this.route.params
+			.switchMap((params: Params) => this.dogService.getDog(+ params["id"]))
+			.subscribe(reply => this.dog = reply);
+	}
+}
