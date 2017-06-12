bf14acd0967ebd547450a97870561c85980ba033
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-12 09:49:02 -0600

    Created about page.

---
+++ static-ui/about.php
@@ -0,0 +1,47 @@
+<!-- HEAD -->
+<?php require_once("library/head-utils.php");?>
+
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- HEADER AND NAVBAR -->
+		<?php require_once("library/header.php");?>
+
+		<main class="class">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-offset-2 col-md-4">
+						<p>LET'S TALK ABOUT BARK PARKZ!</p>
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			<?php require_once("library/footer.php");?>
+		</main>
+	</div>
+</body>
