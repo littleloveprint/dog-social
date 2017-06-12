de0491166487c1d2d73b7b999d31a6809ddd5e4f
Michael Jordan <mikesjordan@gmail.com>
2017-06-12 14:01:08 -0600

    added image component and made a view for image and updated route                                                        https://app.asana.com/0/334403035277159/359198542825358

--- public_html/template/image.php
+++ public_html/template/image.php
@@ -1,1 +1,8 @@
 <h1>Image Component</h1>
+<form class="form-horizontal" name="imageUpload">
+	<div class="form-group">
+		<label for="postImage" class="modal-labels">Upload an image</label>
+		<input type="file" name="dog" id="dog" ng2FileSelect [uploader]="uploader" />
+	</div>
+	<button type="submit" class="btn btn-info btn-lg"><i class="fa fa-file-image-o" aria-hidden="true"></i> Upload Image</button>
+</form>
