0fb52f6f35db7e1d9ce07e5bef2a17e15ccd76bb
Michael Jordan <mikesjordan@gmail.com>
2017-06-12 14:32:42 -0600

    added cookies for angular to package.json                                                        https://app.asana.com/0/334403035277159/359198542825358

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import {ProfileComponent} from "./components/profile.component";
 import {PostService} from "./services/post.service";
 import {ParkComponent} from "./components/park.component";
 import {ImageComponent} from "./components/image.component";
+import {FileSelectDirective} from "ng2-file-upload";
 
 export const allAppComponents = [HomeComponent, SignUpComponent, SignInComponent, NavbarComponent, FooterComponent, ProfileComponent, ParkComponent];
 
