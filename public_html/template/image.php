29fe27d2a72cbee67d9c125655019084cca0ed44
Michael Jordan <mikesjordan@gmail.com>
2017-06-12 14:25:02 -0600

    added cookies for angular to package.json                                                        https://app.asana.com/0/334403035277159/359198542825358

--- public_html/template/image.php
+++ public_html/template/image.php
@@ -1,5 +1,5 @@
 <h1>Image Component</h1>
-<form class="form-horizontal" name="imageUpload">
+<form class="form-horizontal" name="imageUpload" (submit)="uploadImage();">
 	<div class="form-group">
 		<label for="postImage" class="modal-labels">Upload an image</label>
 		<input type="file" name="dog" id="dog" ng2FileSelect [uploader]="uploader" />
--- src/app/components/image.component.ts
+++ src/app/components/image.component.ts
@@ -1,9 +1,40 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FileUploader} from "ng2-file-upload";
+import {Cookie} from "ng2-cookies";
+import {Observable} from "rxjs";
+import "rxjs/add/observable/from";
 
 @Component({
 	templateUrl: "./templates/image.php"
 })
 
-export class ImageComponent {
+export class ImageComponent implements OnInit {
+	public uploader: FileUploader = new FileUploader({
+		url: "./api/image/",
+		headers: [{name: "X-XSRF-TOKEN", value: Cookie.get("XSRF-TOKEN")}],
+		additionalParameter: {}
+	});
+
+	protected cloudinaryPublicId : string = null;
+	protected cloudinaryPublicIdObservable : Observable<string> = null;
+
+	ngOnInit(): void {
+		this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any) => {
+			let reply = JSON.parse(response);
+			// this.cloudinaryPublicId = reply.data;
+			this.cloudinaryPublicIdObservable = Observable.from(this.cloudinaryPublicId);
+			// console.log(this.cloudinaryPublicId);
+			// return Promise.resolve(this.cloudinaryPublicId);
+		};
+	}
+
+	uploadImage() :  void {
+		this.uploader.uploadAll();
+	}
+
+	getCloudinaryId() : void {
+		this.cloudinaryPublicIdObservable
+			.subscribe(cloudinaryPublicId => this.cloudinaryPublicId = cloudinaryPublicId);
+	}
 
 }
