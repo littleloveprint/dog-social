78d82bde42929a6337c227983ef275fa274246ad
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-13 09:31:23 -0600

    Input complete.

--- public_html/template/profile.php
+++ public_html/template/profile.php
@@ -14,9 +14,9 @@
 
 			<!-- TEXT INPUT -->
 			<div class="form-group">
-				<label class="col-md-4 control-label" for="@Handle">@Handle</label>
+				<label class="col-md-4 control-label sr-only" for="@Handle">@Handle</label>
 				<div class="col-md-2">
-					<input id="@Handle" name="@Handle" type="text" placeholder="@Handle" class="form-control input-md">
+					<input id="@Handle" name="@Handle" type="text" placeholder="@Handle" class="form-control input-md" [(ngModel)]="createProfile.profileAtHandle">
 				</div>
 			</div>
 
@@ -24,7 +24,7 @@
 			<div class="form-group">
 				<label class="col-md-4 control-label" for="">Email</label>
 				<div class="col-md-2">
-					<input id="Email" name="Email" type="text" placeholder="Email" class="form-control input-md">
+					<input id="Email" name="Email" type="text" placeholder="Email" class="form-control input-md" [(ngModel)]="createProfile.profileEmail">
 				</div>
 			</div>
 		</form>
