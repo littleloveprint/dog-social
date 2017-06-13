2807c01e30f2d616090f757ea937e8d240a52d7d
Emily Halleran <emily@blueroserevival.com>
2017-06-12 22:30:00 -0600

    Added in more specific google map to park and navbar

--- static-ui/park.php
+++ static-ui/park.php
@@ -1,3 +1,17 @@
+
+<!-- HEAD -->
+<?php require_once("library/head-utils.php");?>
+
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- HEADER AND NAVBAR -->
+		<?php require_once("library/header.php");?>
+
+		<main class="class">
+			<form class="form-horizontal">
+				<fieldset>
+					<!-- Map -->
 <!DOCTYPE html>
 <html>
 	<head>
@@ -10,6 +24,9 @@
 	</head>
 	<body>
 		<h3>Search Parks</h3>
+		<h4>Click on a featured park to get directions!</h4>
+		<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?q=dog%20parks%20near%20Albuquerque%2C%20NM%2C%20United%20States&key=AIzaSyBLthzUq1SMAb5GKh6x60l2Oe3ltFyfHRs" allowfullscreen></iframe>
+
 		<div id="map"></div>
 		<script>
 			function initMap() {
@@ -23,9 +40,6 @@
 					map: map
 				});
 			}
-		</script>
-		<script async defer
-				  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhczI6nGkNcC3Gpe-CJKzRZ-r18s9eyv8&callback=initMap">
 		</script>
 	</body>
 </html>
