ae2915bdfecd65ca90e9ec0a8e29d4000daf0f67
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-13 09:09:28 -0600

    More profile content.

--- public_html/template/profile.php
+++ public_html/template/profile.php
@@ -1,17 +1,28 @@
-<div class="fullscreen-bg">
-	<img src="" alt="Background Image">
-</div>
+<!-- HEAD -->
+<?php require_once("library/head-utils.php"); ?>
+<body class="sfooter">
+	<div class="sfooter-content">
 
-<main class="class">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-offset-2 col-md-4">
-				<p>WE WANT THE CLOUDINARY img HERE</p>
-			</div>
+		<!-- HEADER AND NAV -->
+		<?php require_once("library/header.php"); ?>
+
+		<div class="fullscreen-bg">
+			<img src="" alt="Background Image">
 		</div>
-		<div class="row">
-			<div class="col-md-offset-2 col-md-4">
-				<p id="profile-handle"> Welcome Back To Bark Parkz {{ profile.profileAtHandle }}</p>
+
+		<main class="class">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-offset-2 col-md-4">
+						<p>WE WANT THE CLOUDINARY img HERE</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-offset-2 col-md-4">
+						<p id="profile-handle"> Welcome Back To Bark Parkz {{ profile.profileAtHandle }}</p>
+					</div>
+				</div>
 			</div>
-		</div>
+		</main>
 	</div>
+</body>
