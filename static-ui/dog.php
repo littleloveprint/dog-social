d6aa2605b3eecb6fd00fa0aa2526ff351891b59c
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-06-12 19:20:42 -0600

    added form to dog page including @Handle, Breed, Age, Bio https://app.asana.com/0/332296370771655/359198542825360

--- static-ui/dog.php
+++ static-ui/dog.php
@@ -1,20 +1,62 @@
 <!-- HEAD -->
 <?php require_once("library/head-utils.php");?>
 
-<body class="sfooter" id="dogbackground">
-	<div class="sfooter-content">
+<body>
+
+
 
 		<!-- HEADER AND NAVBAR -->
 		<?php require_once("library/header.php");?>
 
 		<main class="class">
-			CLOUDINARY ID<br>
-			@HANDLE
+			<form class="form-horizontal">
+				<fieldset>
+
+					<!-- Text input-->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="@Handle">@Handle</label>
+						<div class="col-md-4">
+							<input id="@Handle" name="@Handle" type="text" placeholder="@Handle" class="form-control input-md">
+
+						</div>
+					</div>
+
+					<!-- Text input-->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="Age">Age</label>
+						<div class="col-md-2">
+							<input id="Age" name="Age" type="text" placeholder="Age" class="form-control input-md">
+
+						</div>
+					</div>
+
+					<!-- Text input-->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="Breed">Breed</label>
+						<div class="col-md-4">
+							<input id="Breed" name="Breed" type="text" placeholder="Breed" class="form-control input-md">
+
+						</div>
+					</div>
+
+					<!-- Textarea -->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="bio">Bio</label>
+						<div class="col-md-4">
+							<textarea class="form-control" id="bio" name="bio"></textarea>
+						</div>
+						<!-- Button -->
+						<div class="form-group">
+							<label class="col-md-4 control-label" for="submit"></label>
+							<div class="col-md-4">
+								<button id="submit" name="submit" class="btn btn-primary">Submit</button>
+							</div>
+					</div>
 
 
-			<?php require_once("library/footer.php");?>
+
 
 
 		</main>
-	</div>
+
 </body>
