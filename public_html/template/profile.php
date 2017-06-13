ce2031d89ff689a45798477d91e217a8cdff61ea
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-13 09:23:23 -0600

    More input.

--- public_html/template/profile.php
+++ public_html/template/profile.php
@@ -2,25 +2,31 @@
 			<img src="" alt="Background Image">
 		</div>
 
-			<div class="container">
-				<div class="row">
-					<div class="col-md-offset-2 col-md-4">
-						<!-- INSERT CLOUDINARY ID HERE??? -->
-						<p id="profile-handle"> Welcome Back To Bark Parkz {{ profile.profileAtHandle }}</p>
-					</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-offset-2 col-md-4">
 
-					<!-- TEXT INPUT -->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="@Handle">@Handle</label>
-						<div class="col-md-2">
-							<input id="@Handle" name="@Handle" type="text" placeholder="@Handle" class="form-control input-md">
-						</div>
-					</div>
+			<!-- INSERT CLOUDINARY ID HERE??? -->
+			<p id="profile-handle"> Welcome Back To Bark Parkz {{ profile.profileAtHandle }}</p>
+		</div>
 
-					<!-- TEXT INPUT -->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="">Email</label>
-						<div class="col-md-2">
-							<input id="Email" name="Email" type="text" placeholder="Email" class="form-control input-md">
-						</div>
-					</div>
+		<form class="form-horizontal" id="createProfile" name="createProfile" #createProfile="ngForm" (submit)="createProfile">
+
+			<!-- TEXT INPUT -->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="@Handle">@Handle</label>
+				<div class="col-md-2">
+					<input id="@Handle" name="@Handle" type="text" placeholder="@Handle" class="form-control input-md">
+				</div>
+			</div>
+
+			<!-- TEXT INPUT -->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="">Email</label>
+				<div class="col-md-2">
+					<input id="Email" name="Email" type="text" placeholder="Email" class="form-control input-md">
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
