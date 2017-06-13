00c38d3da64a427a80a5bb89d2abd679ad51cfe9
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-06-13 08:42:04 -0600

    copy and pasted dog static ui content into dog template https://app.asana.com/0/332296370771655/359198542825360

--- public_html/template/dog.php
+++ public_html/template/dog.php
@@ -1,17 +1,63 @@
-<div class="fullscreen-bg">
-	<img src="" alt="Background Image">
-</div>
+<!-- HEAD -->
+<?php require_once("library/head-utils.php");?>
 
-<main class="class">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-offset-2 col-md-4">
-				<p>WE WANT THE CLOUDINARY img HERE</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-offset-2 col-md-4">
-				<p id="dog-handle"> WOOF WOOF {{ dog.dogAtHandle }}</p>
-			</div>
-		</div>
-	</div>
+<body>
+
+
+
+	<!-- HEADER AND NAVBAR -->
+	<?php require_once("library/header.php");?>
+
+	<main class="class">
+		<form class="form-horizontal">
+			<fieldset>
+
+				<!-- Text input-->
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="@Handle">@Handle</label>
+					<div class="col-md-2">
+						<input id="@Handle" name="@Handle" type="text" placeholder="@Handle" class="form-control input-md">
+
+					</div>
+				</div>
+
+				<!-- Text input-->
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="Age">Age</label>
+					<div class="col-md-2">
+						<input id="Age" name="Age" type="text" placeholder="Age" class="form-control input-md">
+
+					</div>
+				</div>
+
+				<!-- Text input-->
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="Breed">Breed</label>
+					<div class="col-md-2">
+						<input id="Breed" name="Breed" type="text" placeholder="Breed" class="form-control input-md">
+
+					</div>
+				</div>
+
+				<!-- Textarea -->
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="bio">Bio</label>
+					<div class="col-md-4">
+						<textarea class="form-control" id="bio" name="bio"></textarea>
+					</div>
+				</div>
+				<!-- Button -->
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="submit"></label>
+					<div class="col-md-4">
+						<button id="submit" name="submit" class="btn btn-primary">Submit</button>
+					</div>
+				</div>
+
+
+
+
+
+	</main>
+
+</body>
