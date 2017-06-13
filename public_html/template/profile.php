ed5abd7b1a290df435577fc59837746030ad2638
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-13 09:18:08 -0600

    Restructuring.

--- public_html/template/profile.php
+++ public_html/template/profile.php
@@ -1,28 +1,26 @@
-<!-- HEAD -->
-<?php require_once("library/head-utils.php"); ?>
-<body class="sfooter">
-	<div class="sfooter-content">
-
-		<!-- HEADER AND NAV -->
-		<?php require_once("library/header.php"); ?>
-
-		<div class="fullscreen-bg">
+<div class="fullscreen-bg">
 			<img src="" alt="Background Image">
 		</div>
 
-		<main class="class">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-offset-2 col-md-4">
-						<p>WE WANT THE CLOUDINARY img HERE</p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-offset-2 col-md-4">
+						<!-- INSERT CLOUDINARY ID HERE??? -->
 						<p id="profile-handle"> Welcome Back To Bark Parkz {{ profile.profileAtHandle }}</p>
 					</div>
-				</div>
-			</div>
-		</main>
-	</div>
-</body>
+
+					<!-- TEXT INPUT -->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="@Handle">@Handle</label>
+						<div class="col-md-2">
+							<input id="@Handle" name="@Handle" type="text" placeholder="@Handle" class="form-control input-md">
+						</div>
+					</div>
+
+					<!-- TEXT INPUT -->
+					<div class="form-group">
+						<label class="col-md-4 control-label" for="">Email</label>
+						<div class="col-md-2">
+							<input id="Email" name="Email" type="text" placeholder="Email" class="form-control input-md">
+						</div>
+					</div>
