5ab0f26785ff0630573896cb0bb067b17216e698
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-13 08:56:13 -0600

    Changed footer content.

--- static-ui/library/footer.php
+++ static-ui/library/footer.php
@@ -2,7 +2,9 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-md-offset-0">
-				WE HEART DOGS!
+				<div class="fa fa-copyright" aria-hidden="true">
+					"Bark Parkz 2017"
+				</div>
 			</div>
 		</div>
 	</div>
