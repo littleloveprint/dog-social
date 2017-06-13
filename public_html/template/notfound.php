e49eff44dc4dca855b76294c7c9525d5aa39942c
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-13 15:12:28 -0600

    Created not found page -- needs more "stuff"

---
+++ public_html/template/notfound.php
@@ -0,0 +1,7 @@
+<div class="contaiq">
+	<div class="row">
+		<div class="col-xs-12 text-center">
+			<a routerLink=""><img class="img-responsive" src="img/" alt="404: Page Not Found"></a>
+		</div>
+	</div>
+</div>
