01d86fe873dd8249f9b6b2a5c9e23423c950d418
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-13 09:00:55 -0600

    Footer changes.

--- static-ui/library/footer.php
+++ static-ui/library/footer.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-xs-12 col-md-offset-0">
 				<div class="fa fa-copyright" aria-hidden="true">
-					"Bark Parkz 2017"
+					Bark Parkz 2017
 				</div>
 			</div>
 		</div>
