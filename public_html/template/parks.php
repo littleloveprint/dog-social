713c84402ad36f43b0753f925af57385da424c66
Rochelle Lewis <rlewis37@gmail.com>
2017-06-14 09:57:36 -0600

    @rlewis37 refined parks view

--- public_html/template/parks.php
+++ public_html/template/parks.php
@@ -1,17 +1,23 @@
-			<form class="form-horizontal">
-				<h3>Search Parks</h3>
-				<div class="wrap">
-					<div class="search">
-						<input type="text" class="searchTerm" placeholder="Search for parks in ABQ">
-						<button type="submit" class="searchButton">
-							<i class="fa fa-search"></i>
-						</button>
-					</div>
+<div class="parks-wrap">
+	<navbar></navbar>
+
+	<div class="container">
+		<form class="form-horizontal">
+			<h3>Search Parks</h3>
+			<div class="wrap">
+				<div class="search">
+					<input type="text" class="searchTerm" placeholder="Search for parks in ABQ">
+					<button type="submit" class="searchButton">
+						<i class="fa fa-search"></i>
+					</button>
 				</div>
-			</form>
+			</div>
+		</form>
 
-			<agm-map [latitude] = "lat" [longitude] = "lng">
-				<agm-marker [latitude] = "lat" [longitude] = "lng">
+		<agm-map [latitude]="lat" [longitude]="lng">
+			<agm-marker [latitude]="lat" [longitude]="lng">
 
-				</agm-marker>
-			</agm-map>
+			</agm-marker>
+		</agm-map>
+	</div>
+</div>
