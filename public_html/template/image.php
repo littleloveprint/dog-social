f26d1bf20e32ee822c46a9fa0c01e3a2b5ced4e6
Rochelle Lewis <rlewis37@gmail.com>
2017-06-14 10:06:43 -0600

    @rlewis37 refined image view

--- public_html/template/image.php
+++ public_html/template/image.php
@@ -1,9 +1,16 @@
-<h1>Image Component</h1>
-<form class="form-horizontal" name="imageUpload" (submit)="uploadImage();">
-	<div class="form-group">
-		<label for="postImage" class="modal-labels">Upload an image</label>
-		<input type="file" name="dog" id="dog" ng2FileSelect [uploader]="uploader" />
+<div class="image-wrap">
+
+	<navbar></navbar>
+
+	<div class="container">
+		<h2>Upload An Image</h2>
+		<form name="imageUpload" (submit)="uploadImage();">
+			<div class="form-group">
+				<label for="postImage" class="sr-only">Upload an image</label>
+				<input type="file" name="dog" id="dog" ng2FileSelect [uploader]="uploader" />
+			</div>
+			<button type="submit" class="btn btn-info btn-lg"><i class="fa fa-file-image-o" aria-hidden="true"></i> Upload Image</button>
+		</form>
+		<p class="h5">Cloudinary Public Id: {{ cloudinaryPublicId }}</p>
 	</div>
-	<button type="submit" class="btn btn-info btn-lg"><i class="fa fa-file-image-o" aria-hidden="true"></i> Upload Image</button>
-</form>
-<p>Cloudinary Public Id: {{ cloudinaryPublicId }}</p>
+</div>
