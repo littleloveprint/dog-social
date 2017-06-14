7c6d3d17d0c0db668b582be341f0aa4adf099f71
Rochelle Lewis <rlewis37@gmail.com>
2017-06-14 10:01:28 -0600

    @rlewis37 refined park view - added map

--- public_html/template/park.php
+++ public_html/template/park.php
@@ -1,1 +1,13 @@
-this is the park detail view
+<div class="park-wrap">
+	<navbar></navbar>
+
+	<div class="container">
+		<h3>Park Name</h3>
+
+		<agm-map [latitude]="lat" [longitude]="lng">
+			<agm-marker [latitude]="lat" [longitude]="lng">
+
+			</agm-marker>
+		</agm-map>
+	</div>
+</div>
